b55bcac4d3d931e058ca184fd72c61f9c572efb9
Ivan Liu <ivan.liuyanfeng@gmail.com>
2018-04-30 00:10:24 +1000

    v0.0.1.1

--- src/util/feature_engineering.py
+++ src/util/feature_engineering.py
@@ -186,12 +186,12 @@ def feature_engineering_v1(train_dat, test_dat):
     # cate_cols = ['category_name', 'region', 'city', 'param_1', 'param_2',
     #              'parent_category_name', 'user_type', 'activation_date_dayofweek',
     #              # 'deal_class', 'deal_class_2',
-    #              ['activation_date_dayofweek', 'region']] # user_id
+    #              ['activation_date_dayofweek', 'region']] # user_id, item_id
     # measures = ['mean', 'std', 'quantile', 'skew', 'count']  # , 'kurt'
 
     tgt_cols = ['deal_probability', 'price', 'image_top_1', 'activation_date_weekend']
     cate_cols = ['category_name', 'region', 'city', 'param_1', 'param_2', 'parent_category_name', 'user_type',
-                   'activation_date_dayofweek', 'deal_class', 'deal_class_2', ['activation_date_dayofweek', 'region']]
+                   'activation_date_dayofweek', ['activation_date_dayofweek', 'region']]
     measures = ['mean', 'std', 'quantile', 'skew', 'count']
 
     # dat = dat.sort_values(by=['activation_date'])
