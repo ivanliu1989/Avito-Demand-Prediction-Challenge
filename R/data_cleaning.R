9720cb5acf4d99490641dade4cc1b78cd6056e15
Ivan Liu <ivan.liuyanfeng@gmail.com>
2018-05-01 21:21:31 +1000

    update

---
+++ R/data_cleaning.R
@@ -0,0 +1,9 @@
+library(data.table)
+library(tidyverse)
+train_dat <- read_csv('./data/train.csv')
+# setDT(train_dat)
+# train_dat
+# train_dat <- fread('./data/train.csv', encoding = 'UTF-8')
+
+train_dat$region
+
