da39faa0935338183c811ecd65594e7af7a86f30
Ivan Liu <ivan.liuyanfeng@gmail.com>
2018-06-27 21:16:05 +1000

    fnl

--- R/blending_fnl.R
+++ R/blending_fnl.R
@@ -4,7 +4,7 @@ library(tidyverse)
 
 pred1 = fread("./submissions/fnl/best_public_blend_0.2204.csv")
 pred2 = fread("./submissions/fnl/i_am_back_kernal_blend_22.csv")
-pred3 = fread("./submissions/fnl/user_id_VGG16_cat2vec_sentiment_0.2213.csv") # 0.2228
+pred3 = fread("./submissions/fnl/lgsub_blend.csv") # 0.2228
 
 par(mfcol = c(2,2))
 plot(pred1$deal_probability, pred2$deal_probability)
@@ -16,9 +16,32 @@ identical(pred1$item_id, pred2$item_id)
 identical(pred1$item_id, pred3$item_id)
 
 submit = pred1
-submit$deal_probability = 0.5 * pred1$deal_probability + 0.5 * pred3$deal_probability #+ 0.2 * pred3$deal_probability
+submit$deal_probability = 0.5 * (0.6 * pred1$deal_probability + 0.4 * pred2$deal_probability) + 0.5 * pred3$deal_probability
 
 
-write.csv(submit, file = './submissions/fnl/blend_55_13.csv', row.names = F)
+write.csv(submit, file = './submissions/fnl/blend_433_123.csv', row.names = F)
 
+
+
+
+
+pred1 = fread("./submissions/fnl/lgsub_0.2213.csv") # 0.2228
+pred2 = fread("./submissions/fnl/lgsub_1.csv") # 0.2228
+pred3 = fread("./submissions/fnl/lgsub_2.csv") # 0.2228
+pred4 = fread("./submissions/fnl/lgsub_3.csv") # 0.2228
+pred5 = fread("./submissions/fnl/lgsub_4.csv") # 0.2228
+pred6 = fread("./submissions/fnl/lgsub_5.csv") # 0.2228
+
+par(mfcol = c(2,3))
+plot(pred1$deal_probability, pred2$deal_probability)
+plot(pred1$deal_probability, pred3$deal_probability)
+plot(pred1$deal_probability, pred4$deal_probability)
+plot(pred1$deal_probability, pred5$deal_probability)
+plot(pred1$deal_probability, pred6$deal_probability)
+
+submit = pred1
+submit$deal_probability = pred1$deal_probability + pred2$deal_probability + pred3$deal_probability + pred4$deal_probability + pred5$deal_probability + pred6$deal_probability
+submit$deal_probability = submit$deal_probability / 6
+plot(pred1$deal_probability, submit$deal_probability)
+write.csv(submit, file = './submissions/fnl/lgsub_blend.csv', row.names = F)
 
